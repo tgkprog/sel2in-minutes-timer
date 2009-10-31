2768d76fd7779c8818c365d05c65690cdbfef782
Tushar.Kapila <Tushar.Kapila@379f6486-c656-11de-b2e7-173e0a698186>
2009-10-31 23:58:10 +0000

    version 2.1.1

---
+++ trunk/miti4/bin/copyBats.bat
@@ -0,0 +1,8 @@
+copy /y MinutesTimer_Vb6.exe reminder\
+copy /y MinutesTimer_Vb6.exe ryan\
+copy /y MinutesTimer_Vb6.exe oil\
+copy /y MinutesTimer_Vb6.exe armour\
+copy /y MinutesTimer_Vb6.exe eelaDoor\
+copy /y MinutesTimer_Vb6.exe voice2\
+
+pause
---
+++ trunk/miti4/bin/e-Mail_Developer.bat
@@ -0,0 +1,1 @@
+D:\prog\vb\minuteAlarm\tmr3\bin\e-Mail_Developer.url
