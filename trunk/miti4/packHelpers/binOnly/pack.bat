c19b770d3dc0b307afbe5c5017e4bf17a578337c
Tushar.Kapila@gmail.com <Tushar.Kapila@gmail.com@379f6486-c656-11de-b2e7-173e0a698186>
2009-12-01 14:20:49 +0000

    sync alarm

--- trunk/miti4/packHelpers/binOnly/pack.bat
+++ trunk/miti4/packHelpers/binOnly/pack.bat
@@ -1,21 +1,23 @@
-d:
+g:
 cd\
-rmdir /s/q "D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\bin\"
-mkdir "D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\bin\"
-cd  D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\bin\
+set rootMI=G:\prog\vb\minsTmr\g\h\miti4
+rmdir /s/q "%rootMi%\packBinOnly\bin\"
+mkdir "%rootMi%\packBinOnly\bin\"
+cd  %rootMi%\packBinOnly\bin\
 
+copy /y %rootMi%\src\res\help*.* %rootMi%\bin\res
 rem /h if you want hidden folders too - we don't
-xcopy /c /y/e/v/r/k/q D:\prog\vb\minuteAlarm\g\miti4\bin\*
-rem del /q/f/s D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\bin\res\.svn\*
-rem for /r  "D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\bin\res\.svn" %a  in (.)  do rmdir %a
+xcopy /c /y/e/v/r/k/q %rootMi%\bin\*
+rem del /q/f/s %rootMi%\packBinOnly\bin\res\.svn\*
+rem for /r  "%rootMi%\packBinOnly\bin\res\.svn" %a  in (.)  do rmdir %a
 
 rem not req as hidden folder will not be copied but is good to clean up
-rmdir /s/q "D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\bin\res\.svn"
-rmdir /s/q "D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\bin\.svn"
+rmdir /s/q "%rootMi%\packBinOnly\bin\res\.svn"
+rmdir /s/q "%rootMi%\packBinOnly\bin\.svn"
 
 del /f ..\MinsTimer.zip
-D:\prgFiles\7zip\7z.exe a -tzip -mx9 -pass=20   ..\MinsTimer.zip @D:\prog\vb\minuteAlarm\g\miti4\packBinOnly\pack-list
-rem will run sed now D:\prog\vb\minuteAlarm\g\miti4\Package\single_support\"D:\prog\vb\minuteAlarm\g\miti4\Package\single_support\Minutes_Timer_Installer.sed
+"C:\Program Files\7-Zip\7z.exe" a -tzip -mx9 -pass=20   ..\MinsTimer.zip @%rootMi%\packBinOnly\pack-list
+rem will run sed now %rootMi%\Package\single_support\"%rootMi%\Package\single_support\Minutes_Timer_Installer.sed
 pause
-cd D:\prog\vb\minuteAlarm\g\miti4\Package\single_support\
-"D:\prog\vb\minuteAlarm\g\miti4\Package\single_support\Minutes_Timer_Installer.sed
+cd %rootMi%\Package\single_support\
+"%rootMi%\Package\single_support\Minutes_Timer_Installer.sed
